90ae9bfbcf950f1e8458be909fab5e350daa5d26
Frankie Fu <frankiefu@yahoo.com>
2012-02-15 18:43:14 -0800

    update to use onyx namespace

--- image/source/Image.js
+++ image/source/Image.js
@@ -1,13 +1,9 @@
 enyo.kind({
-	name: "Image",
-	kind: "Control",
+	name: "onyx.Image",
 	tag: "img",
 	attributes: {
 		onload: enyo.bubbler,
 		onerror: enyo.bubbler,
-		// Note: boolean false not working here
-		// In Control.attributeToNode... it removes the attribute if the value is null or false,
-		// that logic seems incorrect.
 		draggable: "false"
 	}
 })
