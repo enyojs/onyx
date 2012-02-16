0e658d55f93f1e61eec64bdeb6a82e19e636cd17
Steve Orvell <steve.orvell@palm.com>
2012-02-16 10:01:47 -0800

    * groupbox-experimental with more complicated, questionable styling.

---
+++ groupbox-experimental/source/Groupbox.js
@@ -0,0 +1,16 @@
+enyo.kind({
+	name: "onyx.Groupbox",
+	classes: "onyx-groupbox",
+	controlClasses: "onyx-groupbox-item",
+});
+
+enyo.kind({
+	name: "onyx.GroupboxHeader",
+	classes: "onyx-groupbox-header"
+});
+
+// FIXME: does this make sense? if so, it doesn't go here. 
+enyo.kind({
+	name: "onyx.ItemDecorator",
+	classes: "onyx-item-decorator enyo-children-inline enyo-children-middle"
+});
---
+++ groupbox-experimental/source/package.js
@@ -0,0 +1,5 @@
+﻿enyo.depends(
+	"Groupbox.css",
+	"Groupbox.js",
+	"GroupboxInput.css"
+);
