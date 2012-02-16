754fc8b2e249d299fa517f71e8ba6ef55b1762b2
Daniel Freedman <compismyrx@gmail.com>
2012-02-15 16:58:44 -0800

    nvm, need disabled handling

--- button/source/Button.js
+++ button/source/Button.js
@@ -2,4 +2,14 @@ enyo.kind({
 	name: "onyx.Button",
 	kind: "enyo.Button",
 	classes: "onyx-button enyo-unselectable",
+	published: {
+		disabled: false
+	},
+	create: function() {
+		this.inherited(arguments);
+		this.disabledChanged();
+	},
+	disabledChanged: function() {
+		this.setAttribute("disabled", this.disabled);
+	}
 });
