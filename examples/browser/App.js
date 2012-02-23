7688b10f100075c3046c18a1d551d4e49605d85c
Steve Orvell <steve.orvell@palm.com>
2012-02-23 10:31:10 -0800

    * updates to browser app; uses layout and adds bookmarks.

--- examples/browser/App.js
+++ examples/browser/App.js
@@ -1,90 +1,126 @@
+var bookmarks = [
+	{title: "Enyo", url: "http://www.enyojs.com"},
+	{title: "TheVerge", url: "http://www.theverge.com"},
+	{title: "Engadget", url: "http://www.endgadget.com"},
+	{title: "Ars Technica", url: "http://www.arstechnica.com"},
+];
+
 enyo.kind({
 	name: "App",
-	classes: "onyx box v",
+	classes: "onyx",
+	style: "background-color: #eaeaea;",
 	components: [
-		{kind: "onyx.Toolbar", classes: "box h center", style: "height: 58px; width: 100%; margin-bottom: 2px;", components: [
-			{kind: "Image", src: "../../resources/grabbutton.png"},
-			{content: "Flexbox"},
-			{kind: "onyx.Button", content: "Back", ontap: "back"},
-			{kind: "onyx.Button", content: "Forward", ontap: "forward"},
-			{kind: "onyx.InputDecorator", classes: "flex1", components: [
-				{kind: "onyx.Input", onchange: "inputChange", style: "width: 100%;", value: "http://www.enyojs.com", defaultFocus: true}
-			]},
-			{kind: "onyx.Button", content: "Go", ontap: "go"}
-		]},
-		{kind: "onyx.Toolbar", style: "margin-bottom: 2px;", components: [
-			{kind: "Image", src: "../../resources/grabbutton.png"},
-			{content: "Unsized"},
-			{kind: "onyx.Button", content: "Back", ontap: "back"},
-			{kind: "onyx.Button", content: "Forward", ontap: "forward"},
-			{kind: "onyx.InputDecorator", components: [
-				{kind: "onyx.Input", onchange: "inputChange", value: "http://www.cnn.com"}
-			]},
-			{kind: "onyx.Button", content: "Go", ontap: "go", input: "input2"}
-		]},
-		{kind: "onyx.Toolbar", style: "position: relative; margin-bottom: 2px;", components: [
-			{kind: "Image", src: "../../resources/grabbutton.png"},
-			{content: "Absolute"},
-			{kind: "onyx.Button", content: "Back", ontap: "back"},
-			{kind: "onyx.Button", content: "Forward", ontap: "forward"},
-			{kind: "onyx.InputDecorator", classes: "enyo-fit", style: "left: 330px; bottom: auto; top: 10px; right: 74px; height: 26px;", components: [
-				{kind: "onyx.Input", onchange: "inputChange", style: "position: absolute; width: 100%;", value: "http://www.amazon.com"}
-			]},
-			{kind: "onyx.Button", content: "Go", style: "position: absolute; right: 0; top: 10px;", ontap: "go", input: "input3"}
-		]},
-		{kind: "onyx.Toolbar", style: "margin-bottom: 2px; overflow: hidden;", components: [
-			{kind: "Image", src: "../../resources/grabbutton.png", style: "float: left; margin-top: 8px;"},
-			{content: "Float", style: "float: left;"},
-			{kind: "onyx.Button", content: "Back", ontap: "back", style: "float: left; margin-top: 8px;"},
-			{kind: "onyx.Button", content: "Forward", ontap: "forward", style: "float: left; margin-top: 8px;"},
-			{kind: "onyx.Button", content: "Go", ontap: "go", input: "input2", style: "float: right; margin-top: 8px;"},
-			{kind: "onyx.InputDecorator", style: "overflow: hidden; display: block; margin-top: 8px;", components: [
-				{kind: "onyx.Input", onchange: "inputChange", value: "http://www.cnn.com"}
-			]}
-		]},
-		{kind: "onyx.Toolbar", style: "margin-bottom: 2px;", components: [
-			{style: "overflow: hidden; width: 100%;", components: [
-				{kind: "Image", src: "../../resources/grabbutton.png", classes: "onyx-toolbar-item", style: "float: left;"},
-				{content: "Float", classes: "onyx-toolbar-item", style: "float: left; line-height: normal;"},
-				{kind: "onyx.Button", content: "Back", ontap: "back", classes: "onyx-toolbar-item", style: "float: left;"},
-				{kind: "onyx.Button", content: "Forward", ontap: "forward", classes: "onyx-toolbar-item", style: "float: left;"},
-				{kind: "onyx.Button", content: "Go", ontap: "go", input: "input2", classes: "onyx-toolbar-item", style: "float: right;"},
-				{kind: "onyx.InputDecorator", classes: "onyx-toolbar-item", style: "overflow: hidden; display: block;", components: [
-					{kind: "onyx.Input", onchange: "inputChange", value: "http://www.cnn.com"}
+		{kind: "Rows", classes: "enyo-fit", components: [
+			{kind: "onyx.Toolbar", components: [
+				{kind: "Image", style: "height: 29px;", src: "../../resources/grabbutton.png", style: "float: left"},
+				{name: "bookmarksButton", kind: "onyx.Button", content: "B", ontap: "toggleBookmarks", style: "float: left;"},
+				{kind: "onyx.Button", content: "&laquo;", ontap: "back", style: "float: left;"},
+				{kind: "onyx.Button", content: "&raquo;", ontap: "forward", style: "float: left;"},
+				{kind: "onyx.Button", content: "Go", ontap: "goTap", input: "input2", style: "float: right;"},
+				{kind: "onyx.InputDecorator", style: "overflow: hidden; display: block;", components: [
+					{kind: "onyx.Input", style: "width: 100%;", onchange: "inputChange", placeholder: "Enter a url", onfocus: "inputFocus"}
 				]}
-			]}
-		]},
-
-
-		{kind: "onyx.Toolbar", style: "margin-bottom: 2px; display: table; width: 100%; line-height: normal;", components: [
-			{kind: "Image", src: "../../resources/grabbutton.png", style: "display: table-cell;"},
-			{content: "Table", style: "display: table-cell;"},
-			{kind: "onyx.Button", content: "Back", ontap: "back", style: "display: table-cell;"},
-			{kind: "onyx.Button", content: "Forward", ontap: "forward", style: "display: table-cell;"},
-			{kind: "onyx.InputDecorator", style: "display: table-cell;", components: [
-				{kind: "onyx.Input", onchange: "inputChange", value: "http://www.cnn.com"}
 			]},
-			{kind: "onyx.Button", content: "Go", ontap: "go", input: "input2", style: "display: table-cell;"}
-		]},
-		{classes: "flex1", style: "position: relative;", components: [
-			{name: "iframe", tag: "iframe", classes: "frame"}
+			{fit: true, kind: "Cols", components: [
+				{name: "bookmarks", kind: "Rows", style: "width: 300px; border-right: 1px solid silver;", components: [
+					{name: "titleDecorator", kind: "onyx.InputDecorator", style: "display: block; margin: 4px; padding: 8px;", components: [
+						{name: "titleInput", kind: "onyx.Input", placeholder: "Enter a title", style: "width: 220px; margin-right: 4px;"},
+						{kind: "onyx.Button", content: "+", ontap: "addBookmark", style: "background-color: lightgreen; color: white; font-weight: bold;"},
+						
+					]},
+					{fit: true, kind: "Scroller", name: "list"},
+					{style: "text-align: right;", components: [
+						{kind: "onyx.Button", content: "-", ontap: "deleteBookmark", style: "margin: 4px; background-color: #ff3333; color: white; font-weight: bold;"}
+					]}
+				]},
+				{fit: true, name: "iframe", tag: "iframe", classes: "frame", onload: "frameload", attributes: {onload: enyo.bubbler}, style: "background: white;"}
+			]}
 		]}
 	],
+	rowItem: {kind: "Control", style: "border-bottom: 1px solid silver; padding: 10px;", ontap: "rowTap"},
 	create: function() {
 		this.inherited(arguments);
-		this.go();
+		// FIXME: label gotcha with button
+		this.$.titleDecorator.setAttribute("for", this.$.titleInput.getId());
+		//
+		this.setBookmarksShowing(this.$.bookmarksButton.active);
 	},
 	inputChange: function(inSender) {
-		this.go();
+		this.goto(inSender.getValue());
 	},
-	go: function(inSender) {
-		var input = inSender && inSender.input ? this.$[inSender.input] : this.$.input;
-		this.$.iframe.setSrc(input.getValue());
+	inputFocus: function(inSender) {
+		if (!inSender.getValue()) {
+			inSender.setValue("http://");
+		}
+	},
+	goto: function(inUrl) {
+		this.$.input.setValue(inUrl);
+		this.$.iframe.setSrc(inUrl);
+	},
+	frameload: function(inSender) {
+		this.log();
+	},
+	goTap: function() {
+		this.goto(this.$.input.getValue());
 	},
 	back: function() {
 		this.$.iframe.hasNode().contentWindow.history.back();
 	},
 	forward: function() {
 		this.$.iframe.hasNode().contentWindow.history.forward();
+	},
+	setBookmarksShowing: function(inShow) {
+		this.$.bookmarks.setShowing(inShow);
+		this.resized();
+	},
+	toggleBookmarks: function(inSender) {
+		inSender.down = !inSender.down;
+		inSender.setActive(inSender.down);
+		inSender.addRemoveClass("active", inSender.active);
+		this.renderBookmarks();
+		this.setBookmarksShowing(inSender.active);
+	},
+	storageItemName: "browser_bookmarks",
+	renderBookmarks: function() {
+		var s = this.storageItemName;
+		this.$.list.destroyClientControls();
+		this.bookmarks = enyo.json.parse(localStorage.getItem(s));
+		if (!this.bookmarks) {
+			this.saveBookmarks(bookmarks);
+			this.bookmarks = bookmarks;
+		}
+		for (var i=0, c; c=this.bookmarks[i]; i++) {
+			this.createComponent(this.rowItem, {container: this.$.list, content: c.title, url: c.url, index: i});
+		}
+		this.$.list.render();
+	},
+	addBookmark: function() {
+		this.log();
+		var t = this.$.titleInput.getValue();
+		var u = this.$.input.getValue();
+		if (t && u) {
+			this.bookmarks.push({title: t, url: u});
+			this.saveBookmarks(this.bookmarks);
+			this.renderBookmarks();
+		}
+	},
+	deleteBookmark: function() {
+		if (this.activated) {
+			enyo.remove(this.bookmarks[this.activated.index], this.bookmarks);
+			this.activated = null;
+			this.saveBookmarks(this.bookmarks);
+			this.renderBookmarks();
+		}
+	},
+	saveBookmarks: function(inBookmarks) {
+		localStorage.setItem(this.storageItemName, enyo.json.stringify(inBookmarks));
+	},
+	rowTap: function(inSender, inEvent) {
+		if (this.activated) {
+			this.activated.applyStyle("background", null);
+		}
+		this.activated = inSender;
+		this.activated.applyStyle("background", "lightblue");
+		this.goto(inSender.url);
 	}
 });
