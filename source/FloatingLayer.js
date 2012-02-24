60bcf09f20e52d1c04caf7095b149cc20bcaf8f1
Steve Orvell <steve.orvell@palm.com>
2012-02-24 13:08:21 -0800

    * Popup (cleanup wip): remove Floater and replace with FloatingLayer.

---
+++ source/FloatingLayer.js
@@ -0,0 +1,24 @@
+enyo.kind({
+	name: "onyx.FloatingLayer",
+	//* @protected
+	create: function() {
+		this.inherited(arguments);
+		this.setParent(null);
+	},
+	render: function() {
+		this.parentNode = document.body;
+		return this.inherited(arguments);
+	},
+	generateInnerHtml: function() {
+		return "";
+	},
+	beforeChildRender: function() {
+		if (!this.hasNode()) {
+			this.render();
+		}
+	},
+	teardownChildren: function() {
+	}
+});
+
+onyx.floatingLayer = new onyx.FloatingLayer();
