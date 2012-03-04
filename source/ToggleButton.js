7cec01af4903e3d54551e92801376d9bd81c6ea8
Martin Kleinschrodt <martin@maklesoft.com>
2012-03-04 11:22:14 +0100

    Give the toggle button the ability to animate between states, i.e. let the knob slide instead of jump between left and right. User can also drag the knob around to change the button state.

--- source/ToggleButton.js
+++ source/ToggleButton.js
@@ -22,7 +22,8 @@ enyo.kind({
 		value: false,
 		onContent: "On",
 		offContent: "Off",
-		disabled: false
+		disabled: false,
+		animated: true
 	},
 	events: {
 		/**
@@ -40,7 +41,8 @@ enyo.kind({
 	components: [
 		{name: "contentOn", classes: "onyx-toggle-content on"},
 		{name: "contentOff", classes: "onyx-toggle-content off"},
-		{classes: "onyx-toggle-button-knob"}
+		{kind: "onyx.Slideable", name: "knob", axis: "h", unit: "px", max: 30, min: 0, overMoving: false,
+			classes: "onyx-toggle-button-knob", ondragfinish: "knobDragFinish"}
 	],
 	create: function() {
 		this.inherited(arguments);
@@ -48,11 +50,20 @@ enyo.kind({
 		this.onContentChanged();
 		this.offContentChanged();
 		this.disabledChanged();
+		this.animatedChanged();
 	},
 	valueChanged: function() {
 		this.addRemoveClass("off", !this.value);
 		this.$.contentOn.setShowing(this.value);
 		this.$.contentOff.setShowing(!this.value);
+		this.calculateSlidingBounds();
+		if (this.animated) {
+			if (this.value) {
+				this.$.knob.animateToMax();
+			} else {
+				this.$.knob.animateToMin();
+			}
+		}
 		this.setActive(this.value);
 	},
 	activeChanged: function() {
@@ -62,13 +73,26 @@ enyo.kind({
 	onContentChanged: function() {
 		this.$.contentOn.setContent(this.onContent || "");
 		this.$.contentOn.addRemoveClass("empty", !this.onContent);
+		this.calculateSlidingBounds();
+		this.valueChanged();
 	},
 	offContentChanged: function() {
 		this.$.contentOff.setContent(this.offContent || "");
 		this.$.contentOff.addRemoveClass("empty", !this.onContent);
+		this.calculateSlidingBounds();
+		this.valueChanged();
 	},
 	disabledChanged: function() {
 		this.addRemoveClass("disabled", this.disabled);
+		this.$.knob.setDraggable(!this.disabled && this.animated);
+	},
+	animatedChanged: function() {
+		this.addRemoveClass("onyx-toggle-button-animated", this.animated);
+		this.$.knob.setDraggable(!this.disabled && this.animated);
+		// When animated is set to false the knobs position should be determined by the css flow attributes
+		if (!this.animated) {
+			this.$.knob.setValue(0);
+		}
 	},
 	updateValue: function(inValue) {
 		if (!this.disabled) {
@@ -79,13 +103,22 @@ enyo.kind({
 	tap: function() {
 		this.updateValue(!this.value);
 	},
+	rendered: function() {
+		// Do this after the Control has been rendered so that the sliding bounds can be calculated properly
+		this.valueChanged();
+	},
+	// Calculate the maximum sliding position of the knob based on the controls bounds
+	calculateSlidingBounds: function() {
+		this.$.knob.setMax(this.getBounds().width-this.$.knob.getBounds().width-2);
+	},
 	dragstart: function(inSender, inEvent) {
 		inEvent.preventNativeDefault();
 		this.dragging = true;
 		this.dragged = false;
 	},
 	drag: function(inSender, inEvent) {
-		if (this.dragging) {
+		// Value shouldn't be updated while the knob is being dragged around
+		if (this.dragging && (!this.animated || inSender != this.$.knob)) {
 			var d = inEvent.dx;
 			if (Math.abs(d) > 10) {
 				this.updateValue(d > 0);
@@ -97,6 +130,13 @@ enyo.kind({
 		this.dragging = false;
 		if (this.dragged) {
 			inEvent.preventTap();
+		}
+	},
+	knobDragFinish: function() {
+		// Toggle the button when the user has finished dragging the knob. Since the Slidable kind always snaps into the opposite position
+		// after dragging it doesn't matter where the knob is at this moment.
+		if (this.animated) {
+			this.updateValue(!this.value);
 		}
 	}
 })
