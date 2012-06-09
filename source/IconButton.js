9600a5c37eb3d2e3538d8b03767f37469d7dc298
unwiredben <combee@techwood.org>
2012-06-09 18:32:50 -0500

    Add documentation about expected 32x64 image size for IconButtons

--- source/IconButton.js
+++ source/IconButton.js
@@ -3,13 +3,17 @@
 
 		{kind: "onyx.IconButton", src: "images/search.png", ontap: "buttonTap"}
 	
-	If an icon should be combined with text inside a button, use an <a href="#onyx.Icon">onyx.Icon</a> inside an <a href="#onyx.Button">onyx.Button</a>.
+	If an icon should be combined with text inside a button, use an 
+	<a href="#onyx.Icon">onyx.Icon</a> inside an <a	href="#onyx.Button">onyx.Button</a>.
 
 		{kind: "onyx.Button", ontap: "buttonTap", components: [
 			{kind: "onyx.Icon", src: "images/search.png"},
 			{content: "Button"}
 		]}
 
+	The image associated with the src property of the onyx.IconButton  is
+	assumed to be 32x64 pixel strip with the top image showing normally, and
+	the bottom one showing with the button is hovered over or active.
 */
 enyo.kind({
 	name: "onyx.IconButton",
