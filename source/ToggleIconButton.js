18811f6d33bb06118624a47b68503bb5fe5519eb
David Greisen <dgreisen@gmail.com>
2012-08-01 19:38:26 -0400

    added ToggleIconButton.js, an icon toggle

---
+++ source/ToggleIconButton.js
@@ -0,0 +1,51 @@
+/**
+	An icon that acts like a toggle switch. The icon image is specified by setting 
+	the *src* property to a URL.
+
+		{kind: "onyx.ToggleIconButton", src: "images/search.png", ontap: "buttonTap"}
+	
+	The image associated with the *src* property of the ToggleIconButton is assumed
+	to be 32x64-pixel strip with the top half showing the button's normal state
+	and the bottom half showing its state when hovered-over or active.
+*/
+
+enyo.kind({
+	name: "onyx.ToggleIconButton",
+	kind: "onyx.Icon",
+	published: {
+		active: false,
+		value: false,
+		disabled: false
+	},
+	classes: "onyx-icon-button onyx-icon-toggle",
+	//* @protected
+	activeChanged: function() {
+		this.bubble("onActivate");
+	},
+	disabledChanged: function() {
+		this.addRemoveClass("disabled", this.disabled);
+	},
+	updateValue: function(inValue) {
+		if (!this.disabled) {
+			this.setValue(inValue);
+		}
+		this.addRemoveClass("active", this.value)
+	},
+	tap: function() {
+		this.updateValue(!this.value);
+		console.log(this.value, this.active)
+	},
+	valueChanged: function() {
+		this.setActive(this.value);
+	},
+	create: function() {
+		this.inherited(arguments);
+		this.value = Boolean(this.value || this.active);
+		this.disabledChanged();
+	},
+	rendered: function() {
+		this.inherited(arguments);
+		this.valueChanged();
+		this.removeClass('onyx-icon')
+	},
+});
