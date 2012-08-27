eb6ef280f8aab20f04f4d32b21a9213931888f47
Jim Tang <jim.tang@palm.com>
2012-08-27 15:29:18 -0700

    Adding link to dev guide article. Enyo-DCO-1.0-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- source/Button.js
+++ source/Button.js
@@ -1,7 +1,8 @@
 /**
-	A button in the onyx style. The color of the button may be customized by
-	applying a background color.
-	
+	_onyx.Button_ is an <a href="#enyo.Button">enyo.Button</a> with Onyx styling
+	applied. The color of the button may be customized by specifying a
+	background color.
+
 	The *onyx-affirmative*, *onyx-negative*, and *onyx-blue* classes provide
 	some built-in presets.
 
@@ -10,6 +11,10 @@
 		{kind: "onyx.Button", content: "Negative", classes: "onyx-negative"},
 		{kind: "onyx.Button", content: "Blue", classes: "onyx-blue"},
 		{kind: "onyx.Button", content: "Custom", style: "background-color: purple; color: #F1F1F1;"}
+
+	For more information, see the documentation on
+	<a href="https://github.com/enyojs/enyo/wiki/Buttons">Buttons</a> in the
+	Enyo Developer Guide.
 */
 enyo.kind({
 	name: "onyx.Button",
