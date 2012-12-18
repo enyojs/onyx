d595b5cf26584b3cad5cd6496391295df85ff9cd
Jim Tang <jim.tang@palm.com>
2012-12-18 15:47:18 -0800

    API comment cleanup for new kind. Enyo-DCO-1.1-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- source/IntegerPicker.js
+++ source/IntegerPicker.js
@@ -1,28 +1,29 @@
 /**
-	_onyx.IntegerPicker_, a subkind of <a href="#onyx.Picker">onyx.Picker</a>, is used to
-	display	a list of integers that can be selected, ranging from _min_ to _max_. It is meant 
-	to be used in conjunction with an <a href="#onyx.PickerDecorator">onyx.PickerDecorator</a>.
-	The decorator loosely couples the picker with an
-	<a href="#onyx.PickerButton">onyx.PickerButton</a>--a button that, when
-	tapped, shows the picker. Once an item is selected, the list of items closes,
-	but the item stays selected and the PickerButton displays the choice that
-	was made.
+    _onyx.IntegerPicker_, a subkind of <a href="#onyx.Picker">onyx.Picker</a>,
+    is used to display a list of integers that can be selected, ranging from
+    _min_ to _max_. It is meant to be used in conjunction with an
+    <a href="#onyx.PickerDecorator">onyx.PickerDecorator</a>. The decorator
+    loosely couples the picker with an
+    <a href="#onyx.PickerButton">onyx.PickerButton</a>--a button that, when
+    tapped, shows the picker. Once an item is selected, the list of items
+    closes,	but the item stays selected and the PickerButton displays the choice
+    that was made.
 
-	To initialize the IntegerPicker to a particular value, set the _value_ property to
-	the integer that should be selected.
-	
-		{kind: "onyx.PickerDecorator", components: [
-			{}, //this uses the defaultKind property of PickerDecorator to inherit from PickerButton
-			{kind: "onyx.IntegerPicker", min: 0, max: 25, value: 5}
-		]}
+    To initialize the IntegerPicker to a particular value, set the _value_
+    property to the integer that should be selected.
 
-	Each item in the list is an <a href="#onyx.MenuItem">onyx.MenuItem</a>, so
-	an _onSelect_ event with the item can be listened to by a client application
-	to determine which picker item was selected.
-	
-	For more information, see the documentation on
-	<a href="https://github.com/enyojs/enyo/wiki/Pickers">Pickers</a> in the
-	Enyo Developer Guide.
+        {kind: "onyx.PickerDecorator", components: [
+            {}, //this uses the defaultKind property of PickerDecorator to inherit from PickerButton
+            {kind: "onyx.IntegerPicker", min: 0, max: 25, value: 5}
+        ]}
+
+    Each item in the list is an <a href="#onyx.MenuItem">onyx.MenuItem</a>, so
+    an application may listen for an _onSelect_ event with the item to determine
+    which picker item was selected.
+
+    For more information, see the documentation on
+    <a href="https://github.com/enyojs/enyo/wiki/Pickers">Pickers</a> in the
+    Enyo Developer Guide.
  */
 enyo.kind({
 	name: "onyx.IntegerPicker",
