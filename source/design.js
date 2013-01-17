5227f3658ef2a8a7260958a1544d3c7da47da683
Mark Bessey <mark.bessey@palm.com>
2013-01-17 15:39:56 -0800

    Remove unused properties, add "description"

--- source/design.js
+++ source/design.js
@@ -1,12 +1,12 @@
 //* @protected
-// This is an in-progress design description of the Onyx controls for use in the Ares designer tool.
+// Design description of the Onyx controls for use in the Ares designer tool.
 Palette.model.push(
 	{name: "onyx", items: [
-		{name: "onyx.Button", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Button", description: "A Pushbutton",
 			inline: {content: "onyx.Button", kind: "onyx.Button"},
 			config: {content: "$name", isContainer: true, kind: "onyx.Button"}
 		},
-		{name: "onyx.InputDecorator", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.InputDecorator", description: "Style an Input",
 			inline: {kind: "onyx.InputDecorator", components: [
 				{kind: "Input", placeholder: "Enter text here"}
 			]},
@@ -14,19 +14,19 @@ Palette.model.push(
 				{kind: "Input", placeholder: "Enter text here"}
 			]}
 		},
-		{name: "onyx.Input", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Input", description: "Text input control",
 			inline: {value: "onyx.Input", kind: "onyx.Input"},
 			config: {kind: "onyx.Input"}
 		},
-		{name: "onyx.ToggleButton", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.ToggleButton", description: "A bushbutton that toggles between active and inactive states",
 			inline: {kind: "onyx.ToggleButton"},
 			config: {kind: "onyx.ToggleButton"}
 		},
-		{name: "onyx.Checkbox", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Checkbox", description: "A checkbox",
 			inline: {kind: "onyx.Checkbox"},
 			config: {kind: "onyx.Checkbox"}
 		},
-		{name: "onyx.RadioGroup",
+		{name: "onyx.RadioGroup", description: "Only one control in a group can be 'active' at a time",
 			inline: {kind: "onyx.RadioGroup", components: [
 				{content: "A"},
 				{content: "B"},
@@ -36,19 +36,19 @@ Palette.model.push(
 				{content: "RadioButton"}
 			]}
 		},
-		{name: "onyx.RadioButton",
+		{name: "onyx.RadioButton", description: "An on-off button, for use with RadioGroup",
 			inline: {content: "RadioButton", kind: "onyx.RadioButton"},
 			config: {content: "$name", kind: "onyx.RadioButton"}
 		},
-		{name: "onyx.Toolbar", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Toolbar", description: "A toolbar",
 			inline: {kind: "onyx.Toolbar"},
 			config: {isContainer: true, kind: "onyx.Toolbar"}
 		},
-		{name: "onyx.Grabber", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Grabber", description: "Graphical 'handle' for dragging",
 			inline: {kind: "onyx.Grabber", style: "background-color: #333; padding: 4px 12px;"},
 			config: {kind: "onyx.Grabber"}
 		},
-		{name: "onyx.Groupbox", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.Groupbox", description: "A container for a group of controls",
 			inline: {kind: "onyx.Groupbox", components: [
 				{content: "Header", kind: "onyx.GroupboxHeader"},
 				{content: "Item", style: "padding: 10px;"}
@@ -58,7 +58,7 @@ Palette.model.push(
 				{content: "Item", style: "padding: 10px;"}
 			]}
 		},
-		{name: "onyx.GroupboxHeader", title: "Foofoofoo", icon: "box_software.png", stars: 4, version: 2.0, blurb: "Use as a simple page header, or add an optional navigation menu.",
+		{name: "onyx.GroupboxHeader", description: "A header for the Groupbox",
 			inline: {content: "Header", kind: "onyx.GroupboxHeader"},
 			config: {content: "$name", kind: "onyx.GroupboxHeader", isContainer: true}
 		}
