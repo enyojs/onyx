4cb4e035b1a901a14766f27b43d95b942eb4f33d
Jason Robitaille <jason@canuckcoding.ca>
2013-02-22 19:20:52 -0600

    Changed the :active:hover workaround for FirefoxOS to be more rebust and far more closely appear like the css selector.

--- source/Slider.js
+++ source/Slider.js
@@ -54,8 +54,11 @@ enyo.kind({
 		//workaround for FirefoxOS which doesn't support :active:hover css selectors
 		//FirefoxOS simulator does :active:hover css selectors, so do additional srcEvent check
 		if(enyo.platform.firefoxOS) {
-			this.moreComponents[2].onenter = "enter";
-			this.moreComponents[2].onleave = "leave";
+			this.moreComponents[2].ondown = "fxosDown";
+			this.moreComponents[2].onenter = "fxosEnter";
+			this.moreComponents[2].ondrag = "fxosDrag";
+			this.moreComponents[2].onleave = "fxosLeave";
+			this.moreComponents[2].onup = "fxosUp";
 		}
 		this.createComponents(this.moreComponents);
 		this.valueChanged();
@@ -106,13 +109,22 @@ enyo.kind({
 			return true;
 		}
 	},
-	enter: function(inSender, inEvent) {
-		if(inEvent.srcEvent.type!="mouseover")
-			this.$.knob.addClass("pressed");
+	fxosDown: function(inSender, inEvent) {
+		this.$.knob.addClass("pressed");
+		this._isInControl = true;
 	},
-	leave: function(inSender, inEvent) {
-		if(inEvent.srcEvent.type!="mouseout")
-			this.$.knob.removeClass("pressed");
+	fxosEnter: function(inSender, inEvent) {
+		this._isInControl = true;
+	},
+	fxosDrag: function(inSender, inEvent) {
+		this.$.knob.addRemoveClass("pressed", this._isInControl);
+	},
+	fxosLeave: function(inSender, inEvent) {
+		this._isInControl = false;
+	},
+	fxosUp: function(inSender, inEvent) {
+		this.$.knob.removeClass("pressed");
+		this._isInControl = false;
 	},
 	//* @public
 	//* Animates to the given value.
