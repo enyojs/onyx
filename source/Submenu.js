05dc33777329e3dd7dc849a949c490aeca46380c
Chris Mondok <chris.mondok@gmail.com>
2013-04-01 23:24:19 -0400

    Adjust Submenu to fit style guide

--- source/Submenu.js
+++ source/Submenu.js
@@ -1,38 +1,49 @@
 /**
- 	_onyx.Submenu_ is a control that exposes a drawer full of menu items. It is
-	meant to be placed inside an <a href="#onyx.Menu">onyx.Menu</a>. When the
-	item is tapped, its submenu items are revealed or hidden.
+	_onyx.Submenu_ is a control that collapses several menu items into a drawer,
+	which can be opened and closed by tapping on its label. It is meant to be
+	placed inside an <a href="#onyx.Menu">onyx.Menu</a>.
+
+		{kind: "onyx.MenuDecorator", components:[
+			{content: "Open menu"},
+			{kind: "onyx.Menu", components:[
+				{content: "One"},
+				{content: "Two"},
+				{kind: "onyx.Submenu", content: "Sort by...", components: [
+					{content: "A"},
+					{content: "B"},
+					{content: "C"},
+				]},
+				{content: "Three"},
+			]},
+		]},
  */
 enyo.kind({
-	name:"onyx.Submenu",
-	defaultKind:"onyx.MenuItem",
-	initComponents: function()
-	{
+	name: "onyx.Submenu",
+	defaultKind: "onyx.MenuItem",
+	initComponents: function() {
 		var owner = this.getInstanceOwner();
 		this.createChrome([
 			{
-				name:"label",
-				kind:"enyo.Control",
-				classes:"onyx-menu-item",
-				content:this.content || this.name,
-				isChrome:true,
-				ontap:"toggleOpen"
+				name: "label",
+				kind: "enyo.Control",
+				classes: "onyx-menu-item",
+				content: this.content || this.name,
+				isChrome: true,
+				ontap: "toggleOpen"
 			},
-			{kind:"onyx.Drawer", name:"client", classes:"client onyx-submenu", isChrome:true, open:false},
+			{kind: "onyx.Drawer", name: "client", classes: "client onyx-submenu", isChrome: true, open: false},
 		]);
 
 		this.inherited(arguments);
 	},
-	toggleOpen:function()
-	{
+	//* @public
+	toggleOpen: function() {
 		this.setOpen(!this.getOpen());
 	},
-	setOpen:function(open)
-	{
+	setOpen: function(open) {
 		this.$.client.setOpen(open);
 	},
-	getOpen:function()
-	{
+	getOpen: function() {
 		return this.$.client.getOpen();
 	}
 });
