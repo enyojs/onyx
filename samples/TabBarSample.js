39c264fdcf46b284732de816fe4855f47894d889
Dominique Dumont <dominique.dumont@hp.com>
2013-05-16 17:45:53 +0200

    ENYO-2310: added dynamic tab bar sample

--- samples/TabBarSample.js
+++ samples/TabBarSample.js
@@ -1,13 +1,18 @@
 enyo.kind(
 	{
-		name: "App",
+		name: "SimpleTabBar",
 		fit: true,
 		components: [
 			{name:"bar",kind: "onyx.TabBar"},
 			{
-				name: 'stuff',
-				content: 'empty',
-				style: "border: 2px grooved black; width 600px; height: 100px; "
+				style: "border: 2px solid grey; ",
+				components: [
+					{
+						content: 'Only the content of this kind is changed',
+						style: 'padding: 1em'
+					},
+					{name: 'stuff', content: 'empty', style: 'padding: 1em'}
+				]
 			}
 		],
 
@@ -41,16 +46,86 @@ enyo.kind(
 
 enyo.kind(
 	{
+		name: "DynamicTabBar",
+		fit: true,
+		components: [
+			{name:"bar",kind: "onyx.TabBar"},
+			{
+				style: "border: 2px solid grey; ",
+				components: [
+					{
+						content: 'create many tabs and reduce the width of the browser',
+					},
+					{name: 'stuff', content: 'empty', style: 'padding: 1em'},
+					{
+						kind: 'onyx.Button', content: 'create tab', ontap: 'addATab',
+						style: 'margin: 0.5em'
+					},
+					{
+						kind: 'onyx.Button', content: 'kill last tab', ontap: 'killTab'
+					}
+				]
+			}
+		],
+
+		handlers: {
+			onTabChanged: "switchStuff"
+		},
+
+		number: 1,
+		create: function() {
+			this.inherited(arguments);
+			var date = new Date();
+			this.creationTime = date.getTime();
+			this.addATab() ;
+		},
+
+		addATab: function(inSender,inEvent) {
+			this.log("adding a tab");
+			var date = new Date();
+			var delta = ( date.getTime() - this.creationTime ) / 1000 ;
+			this.$.bar.addTab(
+				{
+					'caption': 'Tab label ' + this.number++ ,
+					data: { msg: "tab created after " + delta + " seconds" }
+				}
+			) ;
+		},
+
+		switchStuff: function(inSender,inEvent) {
+			this.log("Tapped tab with caption "+ inEvent.caption
+					 + " and message " + inEvent.data.msg );
+			this.$.stuff.setContent( inEvent.data.msg);
+		},
+		killTab: function(inSender,inEvent) {
+			this.log("killing tab");
+			this.$.bar.removeTab({index: this.number-- - 2});
+		}
+
+	}
+);
+
+enyo.kind(
+	{
 		name: "onyx.sample.TabBarSample",
 		classes: "onyx onyx-sample",
 		components: [
 			{
 				classes: "onyx-sample-divider",
-				content: "Tab Bar"
+				content: "Simple Tab Bar"
 			},
 			{
-				kind:"App"
+				kind:"SimpleTabBar"
+			},
+			{
+				classes: "onyx-sample-divider",
+				content: "Dynamic Tab Bar",
+				style: 'padding-top: 4em;'
+			},
+			{
+				kind:"DynamicTabBar"
 			}
 
 	]
 });
+
