15fcebdd42cb7c473c814dbcb4f45f0ecb38d6f0
Dominique Dumont <dominique.dumont@hp.com>
2013-08-29 17:31:19 +0200

    ENYO-2264: reformat

--- source/TabPanels.js
+++ source/TabPanels.js
@@ -19,183 +19,181 @@ Here's an example:
 		new App().renderInto(document.body);
 */
 
-enyo.kind(
-	{
-		name: "onyx.TabPanels",
-		kind: "Panels",
-		//* @protected
-		draggable: false,
+enyo.kind({
+	name: "onyx.TabPanels",
+	kind: "Panels",
+	//* @protected
+	draggable: false,
 
-		handlers  : {
-			onTabChanged: 'switchPanel'
+	handlers  : {
+		onTabChanged: 'switchPanel'
+	},
+
+	/**
+	 * Set a maximum height for the scrollable menu that can be raised on the right of
+	 * the tab bar.
+	 */
+	published: {
+		maxMenuHeight: null,
+	},
+
+	tabTools: [
+		{
+			kind: 'onyx.TabBar',
+			isPanel: true,
+			name: 'bar'
 		},
+		{
+			name: "client",
+			isPanel: true,
+			fit: true,
+			kind: "Panels",
+			classes: "enyo-tab-panels",
+			onTransitionStart: "clientTransitionStart"
+		}
+	],
 
-		/**
-		 * Set a maximum height for the scrollable menu that can be raised on the right of
-		 * the tab bar.
-		 */
-		published: {
-			maxMenuHeight: null,
-		},
+	create: function() {
+		this.inherited(arguments);
+		if (this.debug) {this.log("create called");}
 
-		tabTools: [
-			{
-				kind: 'onyx.TabBar',
-				isPanel: true,
-				name: 'bar'
-			},
-			{
-				name: "client",
-				isPanel: true,
-				fit: true,
-				kind: "Panels",
-				classes: "enyo-tab-panels",
-				onTransitionStart: "clientTransitionStart"
-			}
-		],
+		if (this.getMaxMenuHeight()) {
+			this.maxMenuHeightChanged();
+		}
 
-		create: function() {
-			this.inherited(arguments);
-			if (this.debug) {this.log("create called");}
+		// getPanels called on client will return panels of *this* kind
+		this.$.client.getPanels = this.bindSafely("getClientPanels");
 
-			if (this.getMaxMenuHeight()) {
-				this.maxMenuHeightChanged();
-			}
+		// basically, set all these Panel parameters to false
+		this.draggableChanged();
+		this.animateChanged();
+		this.wrapChanged();
+	},
 
-			// getPanels called on client will return panels of *this* kind
-			this.$.client.getPanels = this.bindSafely("getClientPanels");
+	maxMenuHeightChanged: function() {
+		this.$.bar.setMaxMenuHeight(this.getMaxMenuHeight()) ;
+	},
+	initComponents: function() {
+		if (this.debug) {this.log("initComponents called");}
+		this.createChrome(this.tabTools);
+		this.inherited(arguments);
+		if (this.debug) {this.log("initComponents done");}
+	},
+	getClientPanels: function() {
+		return this.getPanels();
+	},
 
-			// basically, set all these Panel parameters to false
-			this.draggableChanged();
-			this.animateChanged();
-			this.wrapChanged();
-		},
+	flow: function() {
+		this.inherited(arguments);
+		this.$.client.flow();
+	},
+	reflow: function() {
+		this.inherited(arguments);
+		this.$.client.reflow();
+	},
+	draggableChanged: function() {
+		this.$.client.setDraggable(this.draggable);
+		this.draggable = false;
+	},
+	animateChanged: function() {
+		this.$.client.setAnimate(this.animate);
+		this.animate = false;
+	},
+	wrapChanged: function() {
+		this.$.client.setWrap(this.wrap);
+		this.wrap = false;
+	},
 
-		maxMenuHeightChanged: function() {
-			this.$.bar.setMaxMenuHeight(this.getMaxMenuHeight()) ;
-		},
-		initComponents: function() {
-			if (this.debug) {this.log("initComponents called");}
-			this.createChrome(this.tabTools);
-			this.inherited(arguments);
-			if (this.debug) {this.log("initComponents done");}
-		},
-		getClientPanels: function() {
-			return this.getPanels();
-		},
+	isClient: function(inControl) {
+		return ! inControl.isPanel ;
+	},
 
-		flow: function() {
-			this.inherited(arguments);
-			this.$.client.flow();
-		},
-		reflow: function() {
-			this.inherited(arguments);
-			this.$.client.reflow();
-		},
-		draggableChanged: function() {
-			this.$.client.setDraggable(this.draggable);
-			this.draggable = false;
-		},
-		animateChanged: function() {
-			this.$.client.setAnimate(this.animate);
-			this.animate = false;
-		},
-		wrapChanged: function() {
-			this.$.client.setWrap(this.wrap);
-			this.wrap = false;
-		},
+	initDone: false ,
+	rendered: function() {
 
-		isClient: function(inControl) {
-			return ! inControl.isPanel ;
-		},
+		if (this.initDone) { return ;}
 
-		initDone: false ,
-		rendered: function() {
-
-			if (this.initDone) { return ;}
-
-			if (this.debug) {this.log("rendered start");}
-			var that = this ;
-			enyo.forEach(
-				this.controls,
-				function(c) {
-					if (that.isClient(c)) {
-						if (that.debug) {that.log("adding control " + c.name);}
-						that.$.bar.addTab(c) ;
-					}
-				}
-			);
-
-			this.setIndex(this.controls.length - 1);
-			this.initDone = true;
-			if (this.debug) {this.log("rendered done");}
-
-			// must be called at the end otherwise kind size is weird
-			this.inherited(arguments);
-		},
-
-		//* @public
-		/**
-		 *
-		 * Add a new control managed by the tab bar. inControl is a
-		 * control with optional caption attribute. When not specified
-		 * the tab will have a generated caption like 'Tab 0', 'Tab
-		 * 1'. etc...
-		 *
-		 */
-		addTab: function(inControl){
-			this.$.bar.addTab(inControl);
-			this.setIndex(this.controls.length - 1);
-		},
-
-		//* @public
-		/**
-		 *
-		 * Remove a tab from the tab bar. The control managed by the
-		 * tab will also be destroyed. target is an object with either
-		 * a caption attribute or an index. The tab(s) matching the
-		 * caption will be destroyed or the tab with matching index
-		 * will be destroyed.
-		 *
-		 * Example:
-
-			myTab.removeTab({'index':0}); // remove the leftmost tab
-			myTab.removeTab({'caption':'foo.js'});
-
-		 */
-
-		removeTab: function(indexData) {
-			this.$.bar.removeTab(indexData);
-		},
-
-		// layout is a property of inherited UiComponent
-		layoutKindChanged: function() {
-			if (!this.layout) {
-				this.layout = enyo.createFromKind("FittableRowsLayout", this);
-			}
-			this.$.client.setLayoutKind(this.layoutKind);
-		},
-		indexChanged: function() {
-			// FIXME: initialization order problem
-			if (this.$.client.layout) {
-				this.$.client.setIndex(this.index);
-			}
-			this.index = this.$.client.getIndex();
-		},
-		switchPanel: function(inSender, inEvent) {
-			if (this.hasNode()) {
-				var i = inEvent.index;
-				if (this.debug) {
-					this.log("switchPanel called with caption "+ inEvent.caption) ;
-				}
-				if (this.getIndex() != i) {
-					this.setIndex(i);
+		if (this.debug) {this.log("rendered start");}
+		var that = this ;
+		enyo.forEach(
+			this.controls,
+			function(c) {
+				if (that.isClient(c)) {
+					if (that.debug) {that.log("adding control " + c.name);}
+					that.$.bar.addTab(c) ;
 				}
 			}
-		},
-		startTransition: enyo.nop,
-		finishTransition: enyo.nop,
-		stepTransition: enyo.nop,
-		refresh: enyo.nop
-	}
-);
+		);
+
+		this.setIndex(this.controls.length - 1);
+		this.initDone = true;
+		if (this.debug) {this.log("rendered done");}
+
+		// must be called at the end otherwise kind size is weird
+		this.inherited(arguments);
+	},
+
+	//* @public
+	/**
+	 *
+	 * Add a new control managed by the tab bar. inControl is a
+	 * control with optional caption attribute. When not specified
+	 * the tab will have a generated caption like 'Tab 0', 'Tab
+	 * 1'. etc...
+	 *
+	 */
+	addTab: function(inControl){
+		this.$.bar.addTab(inControl);
+		this.setIndex(this.controls.length - 1);
+	},
+
+	//* @public
+	/**
+	 *
+	 * Remove a tab from the tab bar. The control managed by the
+	 * tab will also be destroyed. target is an object with either
+	 * a caption attribute or an index. The tab(s) matching the
+	 * caption will be destroyed or the tab with matching index
+	 * will be destroyed.
+	 *
+	 * Example:
+
+	 myTab.removeTab({'index':0}); // remove the leftmost tab
+	 myTab.removeTab({'caption':'foo.js'});
+
+	*/
+
+	removeTab: function(indexData) {
+		this.$.bar.removeTab(indexData);
+	},
+
+	// layout is a property of inherited UiComponent
+	layoutKindChanged: function() {
+		if (!this.layout) {
+			this.layout = enyo.createFromKind("FittableRowsLayout", this);
+		}
+		this.$.client.setLayoutKind(this.layoutKind);
+	},
+	indexChanged: function() {
+		// FIXME: initialization order problem
+		if (this.$.client.layout) {
+			this.$.client.setIndex(this.index);
+		}
+		this.index = this.$.client.getIndex();
+	},
+	switchPanel: function(inSender, inEvent) {
+		if (this.hasNode()) {
+			var i = inEvent.index;
+			if (this.debug) {
+				this.log("switchPanel called with caption "+ inEvent.caption) ;
+			}
+			if (this.getIndex() != i) {
+				this.setIndex(i);
+			}
+		}
+	},
+	startTransition: enyo.nop,
+	finishTransition: enyo.nop,
+	stepTransition: enyo.nop,
+	refresh: enyo.nop
+});
