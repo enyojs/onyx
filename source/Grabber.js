5c49b7c0fd78a3f311c0d3f1c3f07da466224a0f
Ryan Duffy <ryan.duffy@lge.com>
2014-07-08 16:15:48 -0500

    adds docs for Grabber

--- source/Grabber.js
+++ source/Grabber.js
@@ -1,19 +1,36 @@
-/**
-	_onyx.Grabber_ is a control styled to indicate that an object may be grabbed
-	and moved. It should only be used in this limited context--to indicate that
-	dragging an object will result in movement.
+(function (enyo, scope) {
+	/**
+	* _onyx.Grabber_ is a control styled to indicate that an object may be grabbed
+	* and moved. It should only be used in this limited context--to indicate that
+	* dragging an object will result in movement.
+	* 
+	* ```
+	* 	{kind: 'onyx.Toolbar', components: [
+	* 		{kind: 'onyx.Grabber', ondragstart: 'grabberDragstart',
+	* 			ondrag: 'grabberDrag', ondragfinish: 'grabberDragFinish'},
+	* 		{kind: 'onyx.Button', content: 'More stuff'}
+	* 	]}
+	* ```
+	* 
+	* When using a Grabber inside a Fittable control, be sure to set `noStretch:
+	* true` on the Fittable, or else give it an explicit height. Otherwise, the
+	* Grabber may not be visible.
+	*
+	* @class  onyx.Grabber
+	* @extends enyo.Control
+	* @public
+	*/
+	enyo.kind(
+		/** @lends  onyx.Grabber.prototype */ {
+		
+		/**
+		* @private
+		*/
+		name: 'onyx.Grabber',
 
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Grabber", ondragstart: "grabberDragstart",
-				ondrag: "grabberDrag", ondragfinish: "grabberDragFinish"},
-			{kind: "onyx.Button", content: "More stuff"}
-		]}
-
-	When using a Grabber inside a Fittable control, be sure to set _"noStretch:
-	true"_ on the Fittable, or else give it an explicit height. Otherwise, the
-	Grabber may not be visible.
-*/
-enyo.kind({
-	name: "onyx.Grabber",
-	classes: "onyx-grabber"
-});
+		/**
+		* @private
+		*/
+		classes: 'onyx-grabber'
+	});
+})(enyo, this);
