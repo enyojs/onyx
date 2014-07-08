d29e7be9e1a7444b855c04610ad4a1d08212cd58
Ryan Duffy <ryan.duffy@lge.com>
2014-07-08 13:53:41 -0500

    docs for Drawer

--- source/Drawer.js
+++ source/Drawer.js
@@ -1,13 +1,27 @@
-/**
-	_onyx.Drawer_ is now an empty kind derived from [enyo.Drawer](#enyo.Drawer).
-	All of its functionality has been moved into the latter kind, found in Enyo
-	Core's _ui_ module.
+(function (enyo, scope) {
+	/**
+	* _onyx.Drawer_ is now an empty kind derived from [enyo.Drawer](#enyo.Drawer).
+	* All of its functionality has been moved into the latter kind, found in Enyo
+	* Core's _ui_ module.
+	*
+	* For more information, see the documentation on
+	* [Drawers](building-apps/layout/drawers.html) in the Enyo Developer Guide.
+	*
+	* @class  onyx.Drawer
+	* @extends enyo.Drawer
+	* @public
+	*/
+	enyo.kind(
+		/** @lends onyx.Drawer.prototype */ {
 
-	For more information, see the documentation on
-	[Drawers](building-apps/layout/drawers.html) in the Enyo Developer Guide.
-*/
+		/**
+		* @private
+		*/
+		name: 'onyx.Drawer',
 
-enyo.kind({
-	name: "onyx.Drawer",
-	kind: "enyo.Drawer"
-});
+		/**
+		* @private
+		*/
+		kind: 'enyo.Drawer'
+	});
+})(enyo, this);
