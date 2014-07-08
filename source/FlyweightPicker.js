f5328a643285bfea4236ee60c5ef14bcc6407b00
Ryan Duffy <ryan.duffy@lge.com>
2014-07-08 15:32:44 -0500

    docs for FlyweightPicker

--- source/FlyweightPicker.js
+++ source/FlyweightPicker.js
@@ -1,135 +1,233 @@
-/**
-	_onyx.FlyweightPicker_, a subkind of [onyx.Picker](#onyx.Picker), is a picker
-	that employs the flyweight pattern. It is used to display a large list of
-	selectable items.	As with [enyo.FlyweightRepeater](#enyo.FlyweightRepeater),
-	the _onSetupItem_ event allows for customization of item rendering.
+(function (enyo, scope) {
 
-	To initialize the FlyweightPicker to a particular value, call _setSelected()_
-	with the index of the item you wish to select, and call _setContent()_ with
-	the item that should be shown in the activator button.
+	/**
+	* Fires when a row is being initialized.
+	*
+	* @event onyx.FlyweightPicker#event:onSetupItem
+	* @type {Object}
+	* @property {Number} index - the row index
+	* @property {enyo.Control} flyweight - the row control, for decoration
+	* @see  enyo.FlyweightRepeater#event:onSetupItem
+	* @public
+	*/
 
-	FlyweightPicker will send an _onSelect_ event with a selected item's
-	information. This may be handled by a client application to determine which
-	item was selected.
+	/**
+	* Fires when an item is selected.
+	*
+	* @event onyx.FlyweightPicker#event:onSelect
+	* @type {Object}
+	* @property {String} content - Content of the selected item
+	* @property {Number} selected - Row index of the selected item
+	* @public
+	*/
 
-		enyo.kind({
-			handlers: {
-				onSelect: "itemSelected"
-			},
-			components: [
-				{kind: "onyx.PickerDecorator", components: [
-					{},
-					{name: "yearPicker", kind: "onyx.FlyweightPicker", count: 200,
-						onSetupItem: "setupYear", components: [
-							{name: "year"}
-						]
-					}
-				]}
-			],
-			create: function() {
-				var d = new Date();
-				var y = d.getYear();
-				this.$.yearPicker.setSelected(y);
-				this.$.year.setContent(1900+y);
-			},
-			setupYear: function(inSender, inEvent) {
-				this.$.year.setContent(1900+inEvent.index);
-			},
-			itemSelected: function(inSender, inEvent) {
-				enyo.log("Picker Item Selected: " + inEvent.selected.content);
+	/**
+	* _onyx.FlyweightPicker_, a subkind of {@link onyx.Picker}, is a picker
+	* that employs the flyweight pattern. It is used to display a large list of
+	* selectable items.	As with {@link enyo.FlyweightRepeater},
+	* the {@link onyx.FlyweightPicker#event:onSetupItem} event allows for customization of item
+	* rendering.
+	*
+	* To initialize the FlyweightPicker to a particular value, call `setSelected()`
+	* with the index of the item you wish to select, and call `setContent()` with
+	* the item that should be shown in the activator button.
+	*
+	* FlyweightPicker will send an {@link onyx.FlyweightPicker#event:onSelect} event with a selected
+	* item's information. This may be handled by a client application to determine which
+	* item was selected.
+	*
+	* ```
+	* enyo.kind({
+	* 	handlers: {
+	* 		onSelect: 'itemSelected'
+	* 	},
+	* 	components: [
+	* 		{kind: 'onyx.PickerDecorator', components: [
+	* 			{},
+	* 			{name: 'yearPicker', kind: 'onyx.FlyweightPicker', count: 200,
+	* 				onSetupItem: 'setupYear', components: [
+	* 					{name: 'year'}
+	* 				]
+	* 			}
+	* 		]}
+	* 	],
+	* 	create: function () {
+	* 		var d = new Date();
+	* 		var y = d.getYear();
+	* 		this.$.yearPicker.setSelected(y);
+	* 		this.$.year.setContent(1900+y);
+	* 	},
+	* 	setupYear: function (inSender, inEvent) {
+	* 		this.$.year.setContent(1900+inEvent.index);
+	* 	},
+	* 	itemSelected: function (inSender, inEvent) {
+	* 		enyo.log('Picker Item Selected: ' + inEvent.selected.content);
+	* 	}
+	* })
+	* ```
+	*
+	* @class   onyx.FlyweightPicker
+	* @extends onyx.Picker
+	* @public
+	*/
+	enyo.kind(
+		/** @lends onyx.FlyweightPicker.prototype */ {
+
+		/**
+		* @private
+		*/
+		name: 'onyx.FlyweightPicker',
+
+		/**
+		* @private
+		*/
+		kind: 'onyx.Picker',
+
+		/**
+		* @private
+		*/
+		classes: 'onyx-flyweight-picker',
+
+		/**
+		* @private
+		*/
+		published: {
+			//* Number of rows to render
+			count: 0
+		},
+
+		/**
+		* @private
+		*/
+		events: {
+			onSetupItem: '',
+			/**
+				Fires when an item is selected.
+
+				_inEvent.content_ contains the content of the selected item.
+
+				_inEvent.selected_ contains the row index of the selected item.
+			*/
+			onSelect: ''
+		},
+
+		/**
+		* @private
+		*/
+		handlers: {
+			onSelect: 'itemSelect'
+		},
+
+		/**
+		* @private
+		*/
+		components: [
+			{name: 'scroller', kind: 'enyo.Scroller', strategyKind: 'TouchScrollStrategy', components: [
+				{name: 'flyweight', kind: 'FlyweightRepeater', noSelect: true, ontap: 'itemTap'}
+			]}
+		],
+
+		/**
+		* @private
+		*/
+		scrollerName: 'scroller',
+
+		/**
+		* @private
+		*/
+		initComponents: function () {
+			this.controlParentName = 'flyweight';
+	        this.inherited(arguments);
+			// Force the flyweight's client control (MenuItem is default) to activate. This will
+			// result in a call to processActivatedItem which preps our picker selection logic.
+			// This is a workaround for changes caused by ENYO-1609 which resulted in ENYO-1611.
+			this.$.flyweight.$.client.children[0].setActive(true);
+	    },
+
+		/**
+		* @private
+		*/
+		create: function () {
+			this.inherited(arguments);
+			this.countChanged();
+		},
+
+		/**
+		* @private
+		*/
+		rendered: function () {
+			this.inherited(arguments);
+			this.selectedChanged();
+		},
+
+		/**
+		* Scrolls the {@link onyx.FlyweightPicker#selected selected} control into view
+		*
+		* @public
+		*/
+		scrollToSelected: function () {
+			var n = this.$.flyweight.fetchRowNode(this.selected);
+			this.getScroller().scrollToNode(n, !this.menuUp);
+		},
+
+		/**
+		* @private
+		*/
+		countChanged: function () {
+			this.$.flyweight.count = this.count;
+		},
+
+		/**
+		* @private
+		*/
+		processActivatedItem: function (inItem) {
+			this.item = inItem;
+		},
+
+		/**
+		* @fires onyx.Picker#event:onChange 
+		* @private
+		*/
+		selectedChanged: function (inOld) {
+			if (!this.item) {
+				return;
 			}
-		})
- */
-enyo.kind({
-	name: "onyx.FlyweightPicker",
-	kind: "onyx.Picker",
-	classes: "onyx-flyweight-picker",
-	published: {
-		//* Number of rows to render
-		count: 0
-	},
-	events: {
+			if (inOld != null) {
+				this.item.removeClass('selected');
+				this.$.flyweight.renderRow(inOld);
+			}
+			var n;
+			if (this.selected != null) {
+				this.item.addClass('selected');
+				this.$.flyweight.renderRow(this.selected);
+				// need to remove the class from control to make sure it won't apply to other rows
+				this.item.removeClass('selected');
+				n = this.$.flyweight.fetchRowNode(this.selected);
+			}
+			this.doChange({selected: this.selected, content: n && n.textContent || this.item.content});
+		},
+
 		/**
-			Fires when a row is being initialized.
-
-			_inEvent.index_ contains the row index.
-
-			_inEvent.flyweight_ contains the row control, for decoration.
+		* @fires onyx.FlyweightPicker#event:onSelect
+		* @private
 		*/
-		onSetupItem: "",
+		itemTap: function (inSender, inEvent) {
+			this.setSelected(inEvent.rowIndex);
+			//Send the select event that we want the client to receive.
+			this.doSelect({selected: this.item, content: this.item.content});
+		},
+
 		/**
-			Fires when an item is selected.
-
-			_inEvent.content_ contains the content of the selected item.
-
-			_inEvent.selected_ contains the row index of the selected item.
+		* Block all select events that aren't coming from this control. This is to prevent
+		* select events from MenuItems since they won't have the correct value in a Flyweight
+		* context.
+		* 
+		* @private
 		*/
-		onSelect: ""
-	},
-	//* @protected
-	handlers: {
-		onSelect: "itemSelect"
-	},
-	components: [
-		{name: "scroller", kind: "enyo.Scroller", strategyKind: "TouchScrollStrategy", components: [
-			{name: "flyweight", kind: "FlyweightRepeater", noSelect: true, ontap: "itemTap"}
-		]}
-	],
-	scrollerName: "scroller",
-	initComponents: function() {
-		this.controlParentName = 'flyweight';
-        this.inherited(arguments);
-		//Force the flyweight's client control (MenuItem is default) to activate. This will
-		//result in a call to processActivatedItem which preps our picker selection logic.
-		//This is a workaround for changes caused by ENYO-1609 which resulted in ENYO-1611.
-		this.$.flyweight.$.client.children[0].setActive(true);
-    },
-	create: function() {
-		this.inherited(arguments);
-		this.countChanged();
-	},
-	rendered: function() {
-		this.inherited(arguments);
-		this.selectedChanged();
-	},
-	scrollToSelected: function() {
-		var n = this.$.flyweight.fetchRowNode(this.selected);
-		this.getScroller().scrollToNode(n, !this.menuUp);
-	},
-	countChanged: function() {
-		this.$.flyweight.count = this.count;
-	},
-	processActivatedItem: function(inItem) {
-		this.item = inItem;
-	},
-	selectedChanged: function(inOld) {
-		if (!this.item) {
-			return;
+		itemSelect: function (inSender, inEvent) {
+			if (inEvent.originator != this) {
+				return true;
+			}
 		}
-		if (inOld != null) {
-			this.item.removeClass("selected");
-			this.$.flyweight.renderRow(inOld);
-		}
-		var n;
-		if (this.selected != null) {
-			this.item.addClass("selected");
-			this.$.flyweight.renderRow(this.selected);
-			// need to remove the class from control to make sure it won't apply to other rows
-			this.item.removeClass("selected");
-			n = this.$.flyweight.fetchRowNode(this.selected);
-		}
-		this.doChange({selected: this.selected, content: n && n.textContent || this.item.content});
-	},
-	itemTap: function(inSender, inEvent) {
-		this.setSelected(inEvent.rowIndex);
-		//Send the select event that we want the client to receive.
-		this.doSelect({selected: this.item, content: this.item.content});
-	},
-	itemSelect: function(inSender, inEvent) {
-		//Block all select events that aren't coming from this control. This is to prevent
-		// select events from MenuItems since they won't have the correct value in a Flyweight context.
-		if (inEvent.originator != this) {
-			return true;
-		}
-	}
-});
+	});
+})(enyo, this);
