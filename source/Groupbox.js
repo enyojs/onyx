116a5634baf9f24d7f3cb633978ba582475e8948
Ryan Duffy <ryan.duffy@lge.com>
2014-07-08 16:16:07 -0500

    adds docs for Groupbox and GroupboxHeader

--- source/Groupbox.js
+++ source/Groupbox.js
@@ -1,41 +1,62 @@
-/**
-	_onyx.Groupbox_ displays rows of controls as a vertically-stacked group. It
-	is designed to have container controls as its children, with each container
-	representing a row in the Groupbox.
+(function (enyo, scope) {
+	/**
+	* _onyx.Groupbox_ displays rows of controls as a vertically-stacked group. It
+	* is designed to have container controls as its children, with each container
+	* representing a row in the Groupbox.
+	*
+	* To add a header, specify an {@link onyx.GroupboxHeader) as the
+	* first control in the Groupbox, e.g.:
+	*
+	* ```
+	* {kind: 'onyx.Groupbox', components: [
+	* 	{kind: 'onyx.GroupboxHeader', content: 'Sounds'},
+	* 		{components: [
+	* 			{content: 'System Sounds'},
+	* 			{kind: 'onyx.ToggleButton', value: true}
+	* 		]},
+	* 		{kind: 'onyx.InputDecorator', components: [
+	* 			{kind: 'onyx.Input'}
+	* 		]}
+	* 	]}
+	* ]}
+	* ```
+	*
+	* @class  onyx.Groupbox
+	* @extends enyo.Control
+	* @public
+	*/
+	enyo.kind(
+		/** @lends onyx.Groupbox.prototype */ {
+		
+		/**
+		 * @private
+		 */
+		name: 'onyx.Groupbox',
 
-	To add a header, specify an [onyx.GroupboxHeader](#onyx.GroupboxHeader) as the
-	first control in the Groupbox, e.g.:
+		/**
+		 * @private
+		 */
+		classes: 'onyx-groupbox'
+	});
 
-		{kind: "onyx.Groupbox", components: [
-			{kind: "onyx.GroupboxHeader", content: "Sounds"},
-				{components: [
-					{content: "System Sounds"},
-					{kind: "onyx.ToggleButton", value: true}
-				]},
-				{kind: "onyx.InputDecorator", components: [
-					{kind: "onyx.Input"}
-				]}
-			]}
-		]}
-
-*/
-enyo.kind({
-	name: "onyx.Groupbox",
-	classes: "onyx-groupbox"
-});
-
-/**
-	_onyx.GroupboxHeader_ is a control designed to be placed inside an
-	[onyx.Groupbox](#onyx.Groupbox). When a header is desired, make a
-	GroupboxHeader the first control inside the Groupbox.
-
-		{kind: "onyx.Groupbox", components: [
-			{kind: "onyx.GroupboxHeader", content: "Sounds"},
-			{content: "Yawn"},
-			{content: "Beep"}
-		]}
-*/
-enyo.kind({
-	name: "onyx.GroupboxHeader",
-	classes: "onyx-groupbox-header"
-});
+	/**
+	* _onyx.GroupboxHeader_ is a control designed to be placed inside an {@link onyx.Groupbox}.
+	* When a header is desired, make a GroupboxHeader the first control inside the Groupbox.
+	*
+	* ```
+	* {kind: 'onyx.Groupbox', components: [
+	* 	{kind: 'onyx.GroupboxHeader', content: 'Sounds'},
+	* 	{content: 'Yawn'},
+	* 	{content: 'Beep'}
+	* ]}
+	* ```
+	*
+	* @class  onyx.GroupboxHeader
+	* @extends enyo.Control
+	* @public
+	*/
+	enyo.kind({
+		name: 'onyx.GroupboxHeader',
+		classes: 'onyx-groupbox-header'
+	});
+})(enyo, this);
