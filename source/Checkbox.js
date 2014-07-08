451fcec24bb609f3d00fd19fe25a53c5816571dd
Ryan Duffy <ryan.duffy@lge.com>
2014-07-08 13:46:52 -0500

    docs for Checkbox

--- source/Checkbox.js
+++ source/Checkbox.js
@@ -1,34 +1,70 @@
-/**
-	_onyx.Checkbox_ is a box that shows or hides a checkmark when clicked. The
-	_onChange_ event is fired when the box is clicked. Call _getValue()_ to
-	retrieve a boolean indicating whether the box is currently checked.
+(function (enyo, scope) {
+	/**
+	 * _onyx.Checkbox_ is a box that shows or hides a checkmark when clicked. The
+	 * _onChange_ event is fired when the box is clicked. Call _getValue()_ to
+	 * retrieve a boolean indicating whether the box is currently checked.
+	 * 
+	 * ```
+	 * {kind: 'onyx.Checkbox', onchange: 'checkboxClicked'}
+	 * 
+	 * checkboxClicked: function(inSender) {
+	 * 	if (inSender.getValue()) {
+	 * 		this.log('I've been checked!');
+	 * 	}
+	 * }
+	 * ```
+	 *
+	 * @class  onyx.Checkbox
+	 * @extends enyo.Checkbox
+	 * @public
+	*/
+	enyo.kind(
+		/** @lends  enyo.Checkbox.prototype */ {
 
-		{kind: "onyx.Checkbox", onchange: "checkboxClicked"}
+		/**
+		* @private
+		*/
+		name: 'onyx.Checkbox',
 
-		checkboxClicked: function(inSender) {
-			if (inSender.getValue()) {
-				this.log("I've been checked!");
+		/**
+		* @private
+		*/
+		classes: 'onyx-checkbox',
+
+		/**
+		* @private
+		*/
+		kind: enyo.Checkbox,
+
+		/**
+		* @private
+		*/
+		tag: 'div',
+
+		/**
+		* @private
+		*/
+		handlers: {
+			// prevent double onchange bubble in IE
+			onclick: ''
+		},
+
+		/**
+		* @private
+		*/
+		tap: function(inSender, e) {
+			if (!this.disabled) {
+				this.setChecked(!this.getChecked());
+				this.bubble('onchange');
 			}
-		}
-*/
-enyo.kind({
-	name: "onyx.Checkbox",
-	classes: "onyx-checkbox",
-	//* @protected
-	kind: enyo.Checkbox,
-	tag: "div",
-	handlers: {
-		// prevent double onchange bubble in IE
-		onclick: ""
-	},
-	tap: function(inSender, e) {
-		if (!this.disabled) {
-			this.setChecked(!this.getChecked());
-			this.bubble("onchange");
-		}
-		return !this.disabled;
-	},
-	dragstart: function() {
-		// Override enyo.Input dragstart handler, to allow drags to propagate for Checkbox
-	}
-});
+			return !this.disabled;
+		},
+
+		/**
+		* Override enyo.Input dragstart handler, to allow drags to propagate for Checkbox
+		* 
+		* @private
+		*/
+		dragstart: enyo.nop
+	});
+})(enyo, this);
