8af3d3f278d6b732804da0cab6ae82fc4c72cf1d
Ryan Duffy <ryan.duffy@lge.com>
2014-07-09 09:25:16 -0500

    adds docs for InputDecorator

--- source/InputDecorator.js
+++ source/InputDecorator.js
@@ -1,61 +1,137 @@
-/**
-	_onyx.InputDecorator_ is a control that provides input styling. Any controls
-	in the InputDecorator will appear to be inside an area styled as an	input.
-	Usually, an InputDecorator surrounds an	[onyx.Input](#onyx.Input).
-
-		{kind: "onyx.InputDecorator", components: [
-			{kind: "onyx.Input"}
-		]}
-
-	Other controls, such as buttons, may be placed to the right or left of the
-	input control, e.g.:
-
-		{kind: "onyx.InputDecorator", components: [
-			{kind: "onyx.IconButton", src: "search.png"},
-			{kind: "onyx.Input"},
-			{kind: "onyx.IconButton", src: "cancel.png"}
-		]}
-
-	Note that the InputDecorator fits around the content inside it. If the
-	decorator is sized, then its contents will likely need to be sized as well.
-
-		{kind: "onyx.InputDecorator", style: "width: 500px;", components: [
-			{kind: "onyx.Input", style: "width: 100%;"}
-		]}
-*/
-enyo.kind({
-	name: "onyx.InputDecorator",
-	kind: "enyo.ToolDecorator",
-	tag: "label",
-	classes: "onyx-input-decorator",
-	published:{
-		//* Set to true to make the input look focused when it's not.
-		alwaysLooksFocused:false
-	},
-	//* @protected
-	handlers: {
-		onDisabledChange: "disabledChange",
-		onfocus: "receiveFocus",
-		onblur: "receiveBlur"
-	},
-	create:function() {
-		this.inherited(arguments);
-		this.updateFocus(false);
-	},
-	alwaysLooksFocusedChanged:function(oldValue) {
-		this.updateFocus(this.focus);
-	},
-	updateFocus:function(focus) {
-		this.focused = focus;
-		this.addRemoveClass("onyx-focused", this.alwaysLooksFocused || this.focused);
-	},
-	receiveFocus: function() {
-		this.updateFocus(true);
-	},
-	receiveBlur: function() {
-		this.updateFocus(false);
-	},
-	disabledChange: function(inSender, inEvent) {
-		this.addRemoveClass("onyx-disabled", inEvent.originator.disabled);
-	}
-});
+(function (enyo, scope) {
+	/**
+	* _onyx.InputDecorator_ is a control that provides input styling. Any controls
+	* in the InputDecorator will appear to be inside an area styled as an	input.
+	* Usually, an InputDecorator surrounds an {@link onyx.Input}.
+	*
+	* ```
+	* {kind: 'onyx.InputDecorator', components: [
+	* 	{kind: 'onyx.Input'}
+	* ]}
+	* ```
+	* 
+	* Other controls, such as buttons, may be placed to the right or left of the
+	* input control, e.g.:
+	*
+	* ```
+	* {kind: 'onyx.InputDecorator', components: [
+	* 	{kind: 'onyx.IconButton', src: 'search.png'},
+	* 	{kind: 'onyx.Input'},
+	* 	{kind: 'onyx.IconButton', src: 'cancel.png'}
+	* ]}
+	* ```
+	* 
+	* Note that the InputDecorator fits around the content inside it. If the
+	* decorator is sized, then its contents will likely need to be sized as well.
+	*
+	* ```
+	* {kind: 'onyx.InputDecorator', style: 'width: 500px;', components: [
+	* 	{kind: 'onyx.Input', style: 'width: 100%;'}
+	* ]}
+	* ```
+	* 
+	* @class  onyx.InputDecorator
+	* @extends enyo.ToolDecorator
+	* @public
+	*/
+	enyo.kind(
+		/** @lends  onyx.InputDecorator.prototype */ {
+		
+		/**
+		 * @private
+		 */
+		name: 'onyx.InputDecorator',
+		
+		/**
+		 * @private
+		 */
+		kind: 'enyo.ToolDecorator',
+		
+		/**
+		 * @private
+		 */
+		tag: 'label',
+		
+		/**
+		 * @private
+		 */
+		classes: 'onyx-input-decorator',
+		
+		/**
+		 * @private
+		 */
+		published: {
+			/**
+			 * Set to true to make the input look focused when it's not.
+			 * @type {Boolean}
+			 * @default  false
+			 * @memberOf  onyx.InputDecorator.prototype
+			 * @public
+			 */	
+			alwaysLooksFocused: false
+		},
+		
+		/**
+		 * @private
+		 */
+		handlers: {
+			onDisabledChange: 'disabledChange',
+			onfocus: 'receiveFocus',
+			onblur: 'receiveBlur'
+		},
+		
+		/**
+		 * @private
+		 */
+		create: function () {
+			this.inherited(arguments);
+			this.updateFocus(false);
+		},
+		
+		/**
+		 * @private
+		 */
+		alwaysLooksFocusedChanged: function (oldValue) {
+			this.updateFocus(this.focus);
+		},
+		
+		/**
+		 * Updates the focus state of the control unless
+		 * {@link onyx.InputDecorator#alwaysLooksFocused} is `true`
+		 * 
+		 * @param  {Boolean} focus - Requested focus state
+		 * @private
+		 */
+		updateFocus: function (focus) {
+			this.focused = focus;
+			this.addRemoveClass('onyx-focused', this.alwaysLooksFocused || this.focused);
+		},
+		
+		/**
+		 * Handler for onfocus events triggered by child components
+		 * 
+		 * @private
+		 */
+		receiveFocus: function () {
+			this.updateFocus(true);
+		},
+		
+		/**
+		 * Handler for onblur events triggered by child components
+		 * 
+		 * @private
+		 */
+		receiveBlur: function () {
+			this.updateFocus(false);
+		},
+		
+		/**
+		 * Handler of onDisabledChange events triggered by child components
+		 * 
+		 * @private
+		 */
+		disabledChange: function (inSender, inEvent) {
+			this.addRemoveClass('onyx-disabled', inEvent.originator.disabled);
+		}
+	});
+})(enyo, this);
