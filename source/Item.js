977236c21a9afb4b9f782cf62ec46263d3737aab
Ryan Duffy <ryan.duffy@lge.com>
2014-07-09 10:06:07 -0500

    adds docs for Item

--- source/Item.js
+++ source/Item.js
@@ -1,45 +1,96 @@
-/**
-	_onyx.Item_ is a control designed to display a group of stacked items,
-	typically in lists. By default, items are highlighted when tapped. Set
-	_tapHighlight_ to	_false_ to prevent the highlighting.
-
-		{kind: "onyx.Item", tapHighlight: false}
-*/
-enyo.kind({
-	name: "onyx.Item",
-	classes: "onyx-item",
+(function (enyo, scope) {
 	/**
-		If true, the item will be automatically highlighted (the _onyx-highlight_
-		CSS class will be applied) when tapped. Set to false to disable this
-		behavior.
+	* _onyx.Item_ is a control designed to display a group of stacked items,
+	* typically in lists. By default, items are highlighted when tapped. Set
+	* _tapHighlight_ to	_false_ to prevent the highlighting.
+	*
+	* ```
+	* {kind: 'onyx.Item', tapHighlight: false}
+	* ```
+	*
+	* @class  onyx.Item
+	* @extends enyo.Control
+	* @public
 	*/
-	tapHighlight: true,
-	//* @protected
-	handlers: {
-		onhold: "hold",
-		onrelease: "release"
-	},
-	//* @public
-	hold: function(inSender, inEvent) {
-		if (this.tapHighlight) {
-			onyx.Item.addRemoveFlyweightClass(this.controlParent || this, "onyx-highlight", true, inEvent);
-		}
-	},
-	release: function(inSender, inEvent) {
-		if (this.tapHighlight) {
-			onyx.Item.addRemoveFlyweightClass(this.controlParent || this, "onyx-highlight", false, inEvent);
-		}
-	},
-	//* @protected
-	statics: {
-		addRemoveFlyweightClass: function(inControl, inClass, inTrueToAdd, inEvent, inIndex) {
-			var flyweight = inEvent.flyweight;
-			if (flyweight) {
-				var index = inIndex !== undefined ? inIndex : inEvent.index;
-				flyweight.performOnRow(index, function() {
-					inControl.addRemoveClass(inClass, inTrueToAdd);
-				});
+	enyo.kind(
+		/** @lends  onyx.Item.prototype */ {
+
+		/**
+		* @private
+		*/
+		name: 'onyx.Item',
+
+		/**
+		* @private
+		*/
+		classes: 'onyx-item',
+		
+		published: {
+			/**
+			* If true, the item will be automatically highlighted (the _onyx-highlight_
+			* CSS class will be applied) when tapped. Set to false to disable this
+			* behavior
+			* @type {Boolean}
+			* @default  true
+			* @memberOf  onyx.Item.prototype
+			* @public
+			*/
+			tapHighlight: true
+		},
+
+		/**
+		* @private
+		*/
+		handlers: {
+			onhold: 'hold',
+			onrelease: 'release'
+		},
+
+		/**
+		* @private
+		*/
+		hold: function (inSender, inEvent) {
+			if (this.tapHighlight) {
+				onyx.Item.addRemoveFlyweightClass(this.controlParent || this, 'onyx-highlight', true, inEvent);
+			}
+		},
+
+		/**
+		* @private
+		*/
+		release: function (inSender, inEvent) {
+			if (this.tapHighlight) {
+				onyx.Item.addRemoveFlyweightClass(this.controlParent || this, 'onyx-highlight', false, inEvent);
+			}
+		},
+
+		/**
+		* @private
+		*/
+		statics: {
+			/**
+			* Adds or removes `inClass` to `inControl` based on `inTrueToAdd`
+			* 
+			* @param {enyo.Control} inControl - Control to modify
+			* @param {String} inClass     - CSS Class Name
+			* @param {Boolean} inTrueToAdd - If `true`, the class is added. If `false`, the class
+			* 	is removed
+			* @param {Object} inEvent     - Event object that triggered the call
+			* @param {Number} [inIndex]   - Index of the row in the flyweight. Retrieved from
+			* 	`inEvent` if not specified.
+			*
+			* @memberOf  onyx.Item
+			* @public
+			*/
+			addRemoveFlyweightClass: function (inControl, inClass, inTrueToAdd, inEvent, inIndex) {
+				var flyweight = inEvent.flyweight;
+				if (flyweight) {
+					var index = inIndex !== undefined ? inIndex : inEvent.index;
+					flyweight.performOnRow(index, function() {
+						inControl.addRemoveClass(inClass, inTrueToAdd);
+					});
+				}
 			}
 		}
-	}
-});
+	});
+})(enyo, this);
