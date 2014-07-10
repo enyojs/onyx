6bec36916bbdd585e7de52bfff12e51c0a2f0996
Ryan Duffy <ryan.duffy@lge.com>
2014-07-10 09:01:37 -0500

    adds docs for RadioButton

--- source/RadioButton.js
+++ source/RadioButton.js
@@ -1,12 +1,31 @@
-/**
-	_onyx.RadioButton_ is a radio button designed for use within an
-	[onyx.RadioGroup](#onyx.RadioGroup).
+(function (enyo, scope) {
+	/**
+	* _onyx.RadioButton_ is a radio button designed for use within an {@link onyx.RadioGroup}.
+	* 
+	* For more information, see the documentation on
+	* [Buttons](building-apps/controls/buttons.html) in the Enyo Developer Guide.
+	*
+	* @class  onyx.RadioButton
+	* @extends enyo.Button
+	* @public
+	*/
+	enyo.kind(
+		/** @lends  onyx.RadioButton.prototype */ {
+		
+		/**
+		* @private
+		*/
+		name: 'onyx.RadioButton',
+		
+		/**
+		* @private
+		*/
+		kind: 'enyo.Button',
+		
+		/**
+		* @private
+		*/
+		classes: 'onyx-radiobutton'
+	});
 
-	For more information, see the documentation on
-	[Buttons](building-apps/controls/buttons.html) in the Enyo Developer Guide.
-*/
-enyo.kind({
-	name: "onyx.RadioButton",
-	kind: "enyo.Button",
-	classes: "onyx-radiobutton"
-});
+})(enyo, this);
