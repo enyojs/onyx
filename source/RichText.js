f451c9e3453de9957a4829fe87ac7a64a6466201
Ryan Duffy <ryan.duffy@lge.com>
2014-07-10 09:02:09 -0500

    adds docs for RichText

--- source/RichText.js
+++ source/RichText.js
@@ -1,18 +1,40 @@
-/**
-	_onyx.RichText_ is an Onyx-styled rich text control, derived from
-	[enyo.RichText](#enyo.RichText). Typically, an _onyx.RichText_ is placed
-	inside an [onyx.InputDecorator](#onyx.InputDecorator), which provides styling,
-	e.g.:
+(function (enyo, scope) {
+	/**
+	* _onyx.RichText_ is an Onyx-styled rich text control, derived from
+	* {@link enyo.RichText}. Typically, an _onyx.RichText_ is placed
+	* inside an {@link onyx.InputDecorator}, which provides styling,
+	* e.g.:
+	*
+	* ```
+	* {kind: 'onyx.InputDecorator', components: [
+	* 	{kind: 'onyx.RichText', style: 'width: 100px;', onchange: 'inputChange'}
+	* ]}
+	* ```
+	* 
+	* For more information, see the documentation on [Text
+	* Fields](building-apps/controls/text-fields.html) in the Enyo Developer Guide.
+	*
+	* @class  onyx.RichText
+	* @extends enyo.RichText
+	* @public
+	*/
+	enyo.kind(
+		/** @lends  onyx.RichText.prototype */ {
+		
+		/**
+		* @private
+		*/
+		name: 'onyx.RichText',
+		
+		/**
+		* @private
+		*/
+		kind: 'enyo.RichText',
+		
+		/**
+		* @private
+		*/
+		classes: 'onyx-richtext'
+	});
 
-		{kind: "onyx.InputDecorator", components: [
-			{kind: "onyx.RichText", style: "width: 100px;", onchange: "inputChange"}
-		]}
-
-	For more information, see the documentation on [Text
-	Fields](building-apps/controls/text-fields.html) in the Enyo Developer Guide.
-*/
-enyo.kind({
-	name: "onyx.RichText",
-	kind: "enyo.RichText",
-	classes: "onyx-richtext"
-});
+})(enyo, this);
