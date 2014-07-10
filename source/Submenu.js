efb55ec3c51e36dd3de7f671d20c743e92c06847
Ryan Duffy <ryan.duffy@lge.com>
2014-07-10 10:12:32 -0500

    adds docs for Submenu

--- source/Submenu.js
+++ source/Submenu.js
@@ -1,48 +1,89 @@
-/**
-	_onyx.Submenu_ is a control that collapses several menu items into a drawer,
-	which may be opened and closed by tapping on its label. It is meant to be
-	placed inside an [onyx.Menu](#onyx.Menu).
+(function (enyo, scope) {
+	/**
+	* onyx.Submenu_ is a control that collapses several menu items into a drawer,
+	* hich may be opened and closed by tapping on its label. It is meant to be
+	* laced inside an [onyx.Menu](#onyx.Menu).
+	*
+	* ```
+	* {kind: 'onyx.MenuDecorator', components:[
+	* 	{content: 'Open menu'},
+	* 	{kind: 'onyx.Menu', components:[
+	* 		{content: 'One'},
+	* 		{content: 'Two'},
+	* 		{kind: 'onyx.Submenu', content: 'Sort by...', components: [
+	* 			{content: 'A'},
+	* 			{content: 'B'},
+	* 			{content: 'C'}
+	* 		]},
+	* 		{content: 'Three'}
+	* 	]}
+	* ]}
+	* ```
+	*
+	* @class  onyx.Submenu
+	* @extends enyo.Control
+	* @public
+	*/
+	enyo.kind(
+		/** @lends  onyx.Submenu.prototype */ {
 
-		{kind: "onyx.MenuDecorator", components:[
-			{content: "Open menu"},
-			{kind: "onyx.Menu", components:[
-				{content: "One"},
-				{content: "Two"},
-				{kind: "onyx.Submenu", content: "Sort by...", components: [
-					{content: "A"},
-					{content: "B"},
-					{content: "C"}
-				]},
-				{content: "Three"}
-			]}
-		]}
- */
-enyo.kind({
-	name: "onyx.Submenu",
-	defaultKind: "onyx.MenuItem",
-	initComponents: function() {
-		this.createChrome([
-			{
-				name: "label",
-				kind: "enyo.Control",
-				classes: "onyx-menu-item",
-				content: this.content || this.name,
-				isChrome: true,
-				ontap: "toggleOpen"
-			},
-			{kind: "onyx.Drawer", name: "client", classes: "client onyx-submenu", isChrome: true, open: false}
-		]);
+		/**
+		* @private
+		*/
+		name: 'onyx.Submenu',
 
-		this.inherited(arguments);
-	},
-	//* @public
-	toggleOpen: function() {
-		this.setOpen(!this.getOpen());
-	},
-	setOpen: function(open) {
-		this.$.client.setOpen(open);
-	},
-	getOpen: function() {
-		return this.$.client.getOpen();
-	}
-});
+		/**
+		* @private
+		*/
+		defaultKind: 'onyx.MenuItem',
+
+		/**
+		* @private
+		*/
+		initComponents: function () {
+			this.createChrome([
+				{
+					name: 'label',
+					kind: 'enyo.Control',
+					classes: 'onyx-menu-item',
+					content: this.content || this.name,
+					isChrome: true,
+					ontap: 'toggleOpen'
+				},
+				{kind: 'onyx.Drawer', name: 'client', classes: 'client onyx-submenu', isChrome: true, open: false}
+			]);
+
+			this.inherited(arguments);
+		},
+
+		/**
+		* Toggles the display of the submenu
+		* 
+		* @public
+		*/
+		toggleOpen: function () {
+			this.setOpen(!this.getOpen());
+		},
+
+		/**
+		* Opens or closes the submenu
+		* 
+		* @param {Boolean} open - New state of the submenu
+		* @public
+		*/
+		setOpen: function (open) {
+			this.$.client.setOpen(open);
+		},
+
+		/**
+		* Retrieves the current state of the submenu
+		* 
+		* @return {Boolean} - Current state of the submenu
+		* @public
+		*/
+		getOpen: function () {
+			return this.$.client.getOpen();
+		}
+	});
+
+})(enyo, this);
