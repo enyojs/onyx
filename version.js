878a0759c1d6f3a83bea7d0a11e49d8b9084929c
Ryan Duffy <ryan.duffy@lge.com>
2014-07-16 11:59:39 -0700

    adds namespace

--- version.js
+++ version.js
@@ -1,3 +1,10 @@
+/**
+* Features a variety of commonly used widgets, including toolbars, text inputs, checkboxes, groups
+* and multiple types of buttons. 
+*
+* @namespace onyx 
+*/
+
 if (enyo && enyo.version) {
 	enyo.version.onyx = '2.5.0';
 }
